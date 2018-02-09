184c8c07d50da2aef534f3df3bcc65921e162af9
David Keiter <dkeiter2@unl.edu>
2018-02-09 10:01:50 -0600

    Filtered to remove scale length less than 1mm

--- fish-analysis.R
+++ fish-analysis.R
@@ -3,10 +3,10 @@ library(tidyverse)
 
 fish_data<-read_csv("Gaeta_etal_CLC_data.csv")
 
-fish_data %>% 
-  mutate(length_cat=ifelse(length>300, "big","small")) %>% 
-  filter(!scalelength<1)->
-fish_data_cat
+fish_data %>%         #piping allows modification of a file
+  mutate(length_cat=ifelse(length>300, "big","small")) %>% #mutate adds a column
+  filter(!scalelength<1)-> #filter with an exclamation point removes specified data
+fish_data_cat #creates new object using arrow in row above and object name
 
 
 
