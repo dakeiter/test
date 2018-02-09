52ccc50681c1a05764b0f7d62c764525d29c4f8c
David Keiter <dkeiter2@unl.edu>
2018-02-09 10:31:31 -0600

    Create dataset of just edge measurements for annuli and graph of scalelength as a function of length colored by lakeid

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,5 +1,6 @@
 .libPaths("P:/RLibrary2")
 library(tidyverse)
+library(ggplot2)
 
 fish_data<-read_csv("Gaeta_etal_CLC_data_1.csv")
 
@@ -8,8 +9,11 @@ fish_data %>%         #piping allows modification of a file
   filter(!scalelength<1)-> #filter with an exclamation point removes specified data
 fish_data_cat #creates new object using arrow in row above and object name
 
+fish_data_cat %>% 
+  filter(annnumber=="EDGE")->
+  fish_data_edge
 
-
-
-
+ggplot(data=fish_data_edge, aes(x=length,y=scalelength))+
+  geom_point(aes(color=lakeid))+
+  theme_classic()
 
