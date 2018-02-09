417346ead673082bde5afddbd22f6fc02f2488c8
David Keiter <dkeiter2@unl.edu>
2018-02-09 09:39:16 -0600

    Add category for fish length

--- fish-analysis.R
+++ fish-analysis.R
@@ -3,6 +3,18 @@ library(tidyverse)
 
 fish_data<-read_csv("Gaeta_etal_CLC_data.csv")
 
+fish_data %>% 
+  mutate(length_cat=ifelse(length>200, "big","small")) ->fish_data_cat
+
+
+
+
+
+
+
+
+
+
 
 
 
