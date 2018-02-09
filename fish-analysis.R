b4754be10861c763720cd945d541ecb3ff11054c
David Keiter <dkeiter2@unl.edu>
2018-02-09 09:53:50 -0600

    Removed all data for fish with scalelength of less than 1mm

--- fish-analysis.R
+++ fish-analysis.R
@@ -4,12 +4,10 @@ library(tidyverse)
 fish_data<-read_csv("Gaeta_etal_CLC_data.csv")
 
 fish_data %>% 
-  mutate(length_cat=ifelse(length>300, "big","small")) ->
-fish_data_cat
-
-fish_data_cat %>% 
+  mutate(length_cat=ifelse(length>300, "big","small")) %>% 
   filter(!scalelength<1)->
 fish_data_cat
+
 
 
 
